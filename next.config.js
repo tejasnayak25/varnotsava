40b80406713de12605536d099c637a3c8e479b49
Tejas Nayak <arpithanayak900@gmail.com>
2023-11-13 11:57:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-//@ts-ignore
-global.performance = global.performance || {
-  now: () => new Date().getTime(),
-};
-
 module.exports = nextConfig
